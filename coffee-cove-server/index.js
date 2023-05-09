ac3d542f606c2c67d96c24f57bfa0f64eccedf74
hasankarim18 <hallysust@gmail.com>
2023-05-09 11:49:00 +0600

    credential added in server

--- coffee-cove-server/index.js
+++ coffee-cove-server/index.js
@@ -3,13 +3,52 @@
 
 const express = require('express')
 const cors = require('cors')
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000
 const app = express()
 
 // middlewares 
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+// credentials
+const userName = process.env.DB_USER_NAME;
+const db_password = process.env.DD_PASSWORD;
+
+// const uri =
+ // "mongodb+srv://cocoffeeCove1987:<password>@cluster0.nzfxe6e.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  `mongodb+srv://${userName}:${db_password}@cluster0.nzfxe6e.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 
 app.get('/', (req, res)=> {
